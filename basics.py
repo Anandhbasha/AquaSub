e1974cf6caee4fb8a69c2e4f3fa67265eaba9b32
Anandhbasha <anadhakm@gmail.com>
2025-04-10 18:16:37 +0530

    4/10

---
+++ basics.py
@@ -0,0 +1,28 @@
+# print
+'''Hello
+jhhjkm.m,.m.,
+,,l,l,;l,;l,;l,l,'''
+print('Welcome to Python')
+print('Hello' ,'Every', 'one', sep='-',end="\t")
+# sep
+print("2000","02","22", sep='/' )
+# Data Types
+#Don't use Special Characters
+#Case Sensitive
+#variable name should be start with Letters only
+#Variable name should not contain - or space
+
+    # int    
+    # float
+    # string
+    #boolean
+
+a=100
+b='50.2'
+name= "xyz"
+isalive = True
+
+print(type(a))
+print(type(b))
+print(type(name))
+print(type(isalive))
