e411a60a6f01d08fc93baf7a3596d08b5ed56d32
Anandhbasha <anadhakm@gmail.com>
2025-04-11 18:13:29 +0530

    data types

--- basics.py
+++ basics.py
@@ -2,10 +2,10 @@
 '''Hello
 jhhjkm.m,.m.,
 ,,l,l,;l,;l,;l,l,'''
-print('Welcome to Python')
-print('Hello' ,'Every', 'one', sep='-',end="\t")
-# sep
-print("2000","02","22", sep='/' )
+# print('Welcome to Python')
+# print('Hello' ,'Every', 'one', sep='-',end="\t")
+# # sep
+# print("2000","02","22", sep='/' )
 # Data Types
 #Don't use Special Characters
 #Case Sensitive
@@ -17,12 +17,80 @@ print("2000","02","22", sep='/' )
     # string
     #boolean
 
-a=100
-b='50.2'
-name= "xyz"
-isalive = True
+# a=100
+# b='50.2'
+# name= "xyz"
+# isalive = True
 
-print(type(a))
-print(type(b))
-print(type(name))
-print(type(isalive))
+# print(type(a))
+# print(type(b))
+# print(type(name))
+# print(type(isalive))
+
+# operators
+    #arithmetic operators
+    # +,-,/,*.%,=,**
+# print(2+2*(5+2))
+    
+# print(2**4)
+# #logical operator
+#     # and , or not,not equal
+# a=10
+# b=20
+# # print(a<b and b>a)
+# print(a<b or b<a)
+# print(not(a<b))
+# print(a!=b)
+
+# #argument assignment operator
+#     # =,+=,*=,-=,/=
+# c=50
+# # c=c+10
+# c+=10
+# c*=5
+# print(c)
+# # Membership operator
+# name = "arun"
+# print('k' in name)
+# #identity
+# a=50
+# b=260
+# print(a is b)
+
+# a,b,c,d = 10,"names",True,10.5
+# print(a,b,c,d)
+
+# temp=a
+# a=b
+# a=temp
+# print(a,b)
+# a,b=b,a
+# print(a,b)
+
+#input
+
+# userName = input("Enter Your name")
+# print("Welcome", userName )
+
+# length = float(input("Please Enter the length value:"))
+# width = float(input("Please Enter the width value:"))
+# width = float(input())
+# area = length * width
+# print(area)
+
+##Complex data types
+    # list(value can change),tuple(unable to change the value),range(start,end,increment) - order wise 
+listing = [100,200,300,400]
+print(listing[-1])
+listing[2]="Hello"
+print(listing)
+# tuple
+values = ("yamaha",2012,"cjks895222121")
+print(values)
+ranging = range(10.5,12.5,0.2)
+print(list(ranging))
+# values[0] = "Hero"
+# print(values)
+    # set,dictionary
+
+
